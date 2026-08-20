c9d8ab39293438aab1270da0f9327d34dac53549
zhanwei33 <22679270+zhanwei33@users.noreply.github.com>
2026-08-20 16:05:28 +0800

    [compile-options] remove MLIR root override

--- third_party/ascend/unittest/costmodel_ut/test_compiler_costmodel_contract.py
+++ third_party/ascend/unittest/costmodel_ut/test_compiler_costmodel_contract.py
@@ -44,7 +44,6 @@ class CompilerCostmodelContractTest(unittest.TestCase):
                 "_enable_msdebug",
                 "_get_kernel_target",
                 "_get_llvm_path",
-                "_get_mlir_path",
                 "_get_npucompiler_path",
                 "_get_triton_adapter_opt_path",
                 "_get_triton_mlir_opt_path",
