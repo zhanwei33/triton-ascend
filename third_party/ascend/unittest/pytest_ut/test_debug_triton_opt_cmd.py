bd4473d25ee8274ff80bef0473c6b9e9dc0092a4
zhanwei33 <22679270+zhanwei33@users.noreply.github.com>
2026-08-20 14:38:23 +0800

    [compile-options] remove select analysis option

--- third_party/ascend/unittest/pytest_ut/test_debug_triton_opt_cmd.py
+++ third_party/ascend/unittest/pytest_ut/test_debug_triton_opt_cmd.py
@@ -35,7 +35,6 @@ FAKE_PIPELINE = "builtin.module(some-pass,another-pass)"
 def _make_metadata():
     return {
         "enable_nd2nz_on_vector": False,
-        "enable_select_analysis": False,
         "compile_on_910_95": False,
         "force_simt_template": False,
         "enable_sync_block_lock": False,
