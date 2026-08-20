3bb28a93d97a323c0fa95f743760633ce6df9ec9
zhanwei33 <22679270+zhanwei33@users.noreply.github.com>
2026-08-20 18:53:46 +0800

    [compile-options] centralize compile mode
    (cherry picked from commit b5538d45ed043c43003659259cd134c964ed9eb5)

--- third_party/ascend/include/TritonToLinalg/TritonOpConverter.h
+++ third_party/ascend/include/TritonToLinalg/TritonOpConverter.h
@@ -860,6 +860,16 @@ private:
   static constexpr llvm::StringRef funcNameBase = "triton_indirect_load";
 };
 
+class UnstructuredLoadConverter
+    : public OpConversionPattern<triton::ascend::UnstructuredLoadOp> {
+public:
+  using OpConversionPattern<
+      triton::ascend::UnstructuredLoadOp>::OpConversionPattern;
+  LogicalResult
+  matchAndRewrite(triton::ascend::UnstructuredLoadOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override;
+};
+
 class StrideLoadConverter
     : public OpConversionPattern<triton::ascend::StrideLoadOp> {
 public:
@@ -895,6 +905,16 @@ public:
 
 private:
   static constexpr llvm::StringRef funcNameBase = "triton_indirect_store";
+};
+
+class UnstructuredStoreConverter
+    : public OpConversionPattern<triton::ascend::UnstructuredStoreOp> {
+public:
+  using OpConversionPattern<
+      triton::ascend::UnstructuredStoreOp>::OpConversionPattern;
+  LogicalResult
+  matchAndRewrite(triton::ascend::UnstructuredStoreOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override;
 };
 
 class IndexSelectSimdConverter
